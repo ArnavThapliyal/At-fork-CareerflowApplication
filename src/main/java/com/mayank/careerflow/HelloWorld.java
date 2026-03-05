e26297fe2f708a98a218994f472bb5908360c0f9
Mayank Singh Chouhan <mayanksinghchouhan82@gmail.com>
2026-03-06 01:08:21 +0530

    Project form jobtracker to Careerflow

--- src/main/java/com/mayank/careerflow/HelloWorld.java
+++ src/main/java/com/mayank/careerflow/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.mayank.jobtracker;
+package com.mayank.careerflow;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
